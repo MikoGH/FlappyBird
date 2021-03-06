e4cd3d9bd7c38589ab61d6456d233291554773d0
Miko <unyperm@gmail.com>
2021-03-06 23:54:05 +0500

    up

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class Time():
 		self.clock = pygame.time.Clock()
 		self.time = 0.1
 
-	def timer(self):
+	def timer(self): 
 		''' Считает прошедшее время '''
 		self.clock.tick()
 		self.time += self.clock.get_rawtime()
@@ -35,11 +35,14 @@ class Bird():
 		self.pos = [self.x,self.y]
 		self.width = self.img.get_width()
 		self.height = self.img.get_height()
-		self.radius = self.width//3
-		self.center = [self.x+self.width//2,self.y+self.height//2]
 		self.y0 = self.y
 		self.t_fall = 0
+		self.score = 0
+		self.is_jump = False
 		self.is_alive = True
+		
+		self.timer = Time()
+		self.all_time = Time()
 
 	def rotate_img(self):
 		''' Поворачивает картинку на определенный угол '''
@@ -53,11 +56,6 @@ class Bird():
 		''' Смещает птицу по оси y '''
 		self.t_fall += 1
 		self.y = self.y0 - 22*self.t_fall + self.t_fall**2
-
-	def jump(self):
-		''' Отвечает за прыжок птицы '''
-		self.t_fall = 0
-		self.y0 = self.y
 
 	def choice_image(self):
 		''' Выбирает картинку птицы в зависимости от направления движения. Прыгающая, падающая или летящая прямо '''
@@ -74,31 +72,59 @@ class Bird():
 		for column in columns:
 			if column.x+column.width > self.x and column.x < self.x + self.width:
 				if self.y < column.y1 or self.y+self.height > column.y2:
-					# self.is_alive = False
-					pygame.draw.circle(win,(210,0,0),[self.x+self.width//2,self.y+self.height//2],17)
-			if self.y < 0 or self.y + self.height > win_height:
-				# self.is_alive = False
-				pygame.draw.circle(win,(210,0,0),[self.x+self.width//2,self.y+self.height//2],17)
+					self.is_alive = False
+			if self.y < 0 or self.y + self.height > win_height-ground_height:
+				self.is_alive = False
+
+	def get_data(self,columns):
+		''' Возвращает расстояние до ближайшей колонны, высоту от верхней колонны до птицы, высоту от нижней колонны до птицы '''
+		for column in columns:
+			if column.x+column.width > self.x:
+				return [
+					column.x - self.x,
+					self.y - column.y1,
+					column.y2 - (self.y + self.height)
+					]
+
+	def get_score(self):
+		''' Увеличивает очки живой птицы за пройденное расстояние '''
+		self.score += 0.001
+
+	def jump(self,i,nets,birds,columns):
+		''' Отвечает за прыжок птицы '''
+		n = nets[i].activate(self.get_data(columns))
+		a = n.index(max(n[:2]))
+		if a == 0:
+			self.t_fall = 0
+			self.y0 = self.y
 
 
 class Column():
 	def __init__(self):
-		global win_width,win_height
+		global win_width,win_height,ground_height
 		self.width = 120
 		self.x = win_width
 		self.space_len = 240
-		self.y1 = random.randint(10,win_height-self.space_len-10)
+		self.y1 = random.randint(10,win_height-self.space_len-ground_height-10)
 		self.y2 = self.y1+self.space_len
 		self.len1 = self.y1
-		self.len2 = win_height-self.y2
+		self.len2 = win_height-self.y2-ground_height
 		
 
 	def draw(self,win):
 		''' Выводит на экран изображение колонн '''
 		pygame.draw.rect(win,[20,180,5],(self.x,0,self.width,self.len1))
 		pygame.draw.rect(win,[20,180,5],(self.x,self.y2,self.width,self.len2))
-		pygame.draw.rect(win,[0,0,0],(self.x,0,self.width,self.len1),4)
-		pygame.draw.rect(win,[0,0,0],(self.x,self.y2,self.width,self.len2),4)
+		pygame.draw.rect(win,[30,190,7],(self.x,0,self.width//3,self.len1))
+		pygame.draw.rect(win,[30,190,7],(self.x,self.y2,self.width//3,self.len2))				
+		pygame.draw.rect(win,[60,210,20],(self.x+self.width//12,0,self.width//24,self.len1))
+		pygame.draw.rect(win,[60,210,20],(self.x+self.width//12,self.y2,self.width//24,self.len2))			
+		pygame.draw.rect(win,[15,150,2],(self.x+self.width-self.width//4-self.width//10,0,self.width//14,self.len1))
+		pygame.draw.rect(win,[15,150,2],(self.x+self.width-self.width//4-self.width//10,self.y2,self.width//14,self.len2))	
+		pygame.draw.rect(win,[15,150,2],(self.x+self.width-self.width//4,0,self.width//4,self.len1))
+		pygame.draw.rect(win,[15,150,2],(self.x+self.width-self.width//4,self.y2,self.width//4,self.len2))
+		pygame.draw.rect(win,[0,0,0],(self.x,0,self.width,self.len1),5)
+		pygame.draw.rect(win,[0,0,0],(self.x,self.y2,self.width,self.len2),5)
 		
 def check_need_column(columns):
 	''' Проверяет, пора ли добавить новую колонну. Если да, добавляет её '''
@@ -109,23 +135,45 @@ def check_need_column(columns):
 		columns.pop(0)
 	return columns
 
+def draw_ground(win):
+	''' Выводит на экран изображение земли '''
+	pygame.draw.rect(win,[150,70,5],(0,win_height-ground_height,win_width,ground_height))	
+	pygame.draw.rect(win,[190,100,3],(0,win_height-ground_height,win_width,ground_height//3))	
+	pygame.draw.rect(win,[0,0,0],(0,win_height-ground_height,win_width,ground_height),4)
+
+
 win_width = 800
 win_height = 800
-pygame.init()
+ground_height = 30
+bcground = pygame.image.load('bcground.png')
 win = pygame.display.set_mode((win_width,win_height))
 pygame.display.set_caption('Flappy Bird Neat')
 timer_column = Time()
-timer_bird = Time()
+# timer_bird = Time()
 
-def start():
-	birds = [Bird(i) for i in range(1)]
-	columns = [Column()]
+pygame.init()
+
+def start(genomes,config):
+	global best_score
+	global generation
+	generation += 1
+	nets = []
+	birds = []
+	columns = [Column()]	
+
+	for i, g in genomes:
+		net = neat.nn.FeedForwardNetwork.create(g, config)	
+		g.fitness = 0
+		nets.append(net)
+		birds.append(Bird(i%8))
 
 	run = True
-	jump = False
-	while len(birds) > 0:
+	alives = len(birds)
+	while alives > 0:
 		''' Вывод на экран изображений '''
 		win.fill((9,91,174))
+		win.blit(bcground,(0,win_height-win_height//2))
+		draw_ground(win)
 
 		for column in columns:
 			column.draw(win)
@@ -136,33 +184,45 @@ def start():
 
 		''' Таймеры '''
 		timer_column.timer()
-		timer_bird.timer()
+		for bird in birds:
+			bird.timer.timer()
 
 		''' Смещение объектов '''
 		if timer_column.check_timer(5):
 			for column in columns:
 				column.x -= 1
 
-		for bird in birds:
+		alives = 0
+		for i,bird in enumerate(birds):
 			if bird.is_alive:
+				alives += 1
 				bird.check_collision(columns)
-				if timer_bird.check_timer(30):
+				if bird.timer.check_timer(30):
 					bird.move()
 					bird.choice_image()
-				if jump==True:
-					bird.jump()
+				bird.jump(i,nets,birds,columns)
+				bird.get_score()
+				genomes[i][1].fitness += bird.score
 
 		check_need_column(columns)		
 
 		''' Обработка событий '''
-		jump = False
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				sys.exit()
-			elif event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_SPACE:
-					jump = True 
 
 		pygame.display.update()	
-		
-start()
+
+generation = 0
+best_score = 0
+
+config_path = './config-feedforward.txt'
+config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
+
+p = neat.Population(config)
+
+p.add_reporter(neat.StdOutReporter(True))
+stats = neat.StatisticsReporter()
+p.add_reporter(stats)
+
+p.run(start, 1000)
