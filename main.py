039234eb01afe70b95305b696b30ac3c2317782f
Miko <unyperm@gmail.com>
2021-03-08 19:51:32 +0500

    up

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import pygame
 import neat
-import math
 import random
 import sys
-from PIL import Image
 
 class Time():
 	def __init__(self):
 		self.clock = pygame.time.Clock()
-		self.time = 0.1
+		self.time = 0
 
 	def timer(self): 
 		''' Считает прошедшее время '''
@@ -18,7 +16,7 @@ class Time():
 	def check_timer(self,num):
 		''' Обнуляет счетчик времени и возвращает истину, если достигло указанного времени '''
 		if self.time >= num:
-			self.time = 0.1
+			self.time = 0
 			return True
 		return False
 
@@ -31,7 +29,6 @@ class Bird():
 		self.img = self.img_main
 		self.x = 200
 		self.y = win_height//2
-		self.pos = [self.x,self.y]
 		self.width = self.img.get_width()
 		self.height = self.img.get_height()
 		self.y0 = self.y
@@ -40,13 +37,7 @@ class Bird():
 		self.score_columns = 0
 		self.is_jump = False
 		self.is_alive = True
-		
 		self.timer = Time()
-		self.all_time = Time()
-
-	def rotate_img(self):
-		''' Поворачивает картинку на определенный угол '''
-		self.rot_img = pygame.transform.rotate(self.img,self.angle)
 
 	def draw(self,win):
 		''' Выводит на экран изображение птицы '''
@@ -91,6 +82,7 @@ class Bird():
 		self.score += 0.001
 
 	def get_score_columns(self,columns):
+		''' Считает пройденные колонны '''
 		for column in columns:
 			if self.x == column.x+column.width:
 				self.score_columns += 1
@@ -131,14 +123,12 @@ class Column():
 		pygame.draw.rect(win,[0,0,0],(self.x,0,self.width,self.len1),5)
 		pygame.draw.rect(win,[0,0,0],(self.x,self.y2,self.width,self.len2),5)
 		
+
 def check_need_column(columns):
 	''' Проверяет, пора ли добавить новую колонну. Если да, добавляет её '''
 	global win_width
-	if win_width-columns[-1].x >= 500:
-		columns.append(Column())
-	if columns[0].x+columns[0].width<0:
-		columns.pop(0)
-	return columns
+	if win_width-columns[-1].x >= 500: columns.append(Column())
+	if columns[0].x+columns[0].width<0: columns.pop(0)
 
 def draw_ground(win):
 	''' Выводит на экран изображение земли '''
@@ -146,28 +136,20 @@ def draw_ground(win):
 	pygame.draw.rect(win,[190,100,3],(0,win_height-ground_height,win_width,ground_height//3))	
 	pygame.draw.rect(win,[0,0,0],(0,win_height-ground_height,win_width,ground_height),4)
 
-def print_text(win,text,width,height,size,font = 'Comic Sans MS',color = (0, 0, 0),bg_color = (80,80,80),bg = False,center = True):
+def print_text(win,text,width,height,size,font = 'Comic Sans MS',color = (0,0,0),center = True):
+	''' Выводит на экран текст '''
 	font = pygame.font.SysFont(font, size)
-	text = font.render(text, True, color, bg_color) if bg else font.render(text, True, color)
+	text = font.render(text, True, color)
 	text_rect = text.get_rect()
 	if center == True: text_rect.center = (width, height)
 	else: text_rect.midleft = (width, height)
 	win.blit(text, text_rect)
 
 
-win_width = 800
-win_height = 800
-ground_height = 30
-bcground = pygame.image.load('bcground.png')
-win = pygame.display.set_mode((win_width,win_height))
-pygame.display.set_caption('Flappy Bird Neat')
-timer_column = Time()
-
-pygame.init()
-
 def start(genomes,config):
 	global best_score
 	global generation
+	current_best_score = 0
 	generation += 1
 	nets = []
 	birds = []
@@ -196,9 +178,9 @@ def start(genomes,config):
 		''' Вывод на экран текста '''
 		print_text(win,'Generation: %d'%(generation),10,20,30,center = False)
 		print_text(win,'Best score: %d'%(best_score),10,60,30,center = False)
-		print_text(win,'%d'%(max(bird.score_columns for bird in birds)),win_width//2+2,60+2,60,color = (0,0,0))
-		print_text(win,'%d'%(max(bird.score_columns for bird in birds)),win_width//2-2,60-2,60,color = (0,0,0))
-		print_text(win,'%d'%(max(bird.score_columns for bird in birds)),win_width//2,60,60,color = (255,250,0))		
+		print_text(win,'%d'%(current_best_score),win_width//2+2,60+2,60,color = (0,0,0))
+		print_text(win,'%d'%(current_best_score),win_width//2-2,60-2,60,color = (0,0,0))
+		print_text(win,'%d'%(current_best_score),win_width//2,60,60,color = (255,250,0))		
 
 		''' Таймеры '''
 		timer_column.timer()
@@ -224,7 +206,8 @@ def start(genomes,config):
 				genomes[i][1].fitness += bird.score
 
 		check_need_column(columns)	
-		best_score = max(max(bird.score_columns for bird in birds),best_score)
+		current_best_score = max(bird.score_columns for bird in birds)
+		best_score = max(current_best_score,best_score)
 
 		''' Обработка событий '''
 		for event in pygame.event.get():
@@ -233,8 +216,20 @@ def start(genomes,config):
 
 		pygame.display.update()	
 
+pygame.init()
+
 generation = 0
 best_score = 0
+
+win_width = 800
+win_height = 800
+ground_height = 30
+
+pygame.display.set_caption('Flappy Bird Neat')
+bcground = pygame.image.load('bcground.png')
+win = pygame.display.set_mode((win_width,win_height))
+
+timer_column = Time()
 
 config_path = './config-feedforward.txt'
 config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
